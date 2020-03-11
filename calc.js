3eb62a3f6309797064fa52611396221966689ecf
Gogulaanand R <gogulaanand02@gmail.com>
2020-03-11 20:43:12 +0530

    completed app

--- calc.js
+++ calc.js
@@ -1,16 +1,58 @@
-var disp1 = document.getElementById("numbers_row2");
-var disp2 = document.getElementById("numbers_row1");
+var disp = document.getElementById("numbers_row"),
+		arg1 = 0,
+		arg2 = 0,
+		inverse = false,
+		actor;
 
 function add_to_display(num){
 	var new_num = document.createTextNode(num);
-	disp1.appendChild(new_num);
+	disp.appendChild(new_num);
 }
 
 function clear_scr(){
-	disp1.textContent = '';
+	disp.textContent = '';
+	disp.offsetHeight;
 }
 
 function operator(optr){
-	disp2.appendChild(disp1.innerHTML);
-	disp1.textContent = '';
+	actor = optr;
+	arg1 = Number(disp.textContent);
+	disp.textContent = '';
+}
+
+function negate(){
+	console.log(disp.childNodes);
+	if(!inverse){
+		disp.prepend('-');
+		inverse = true;
+	}
+	else{
+		var new_text = (disp.textContent).replace('-','');
+		disp.textContent = new_text;
+		inverse = false;
+	}
+}
+
+function eval(){
+	var result;
+	arg2 = Number(disp.textContent);
+	console.log(typeof(arg1));
+	console.log(typeof(arg2));
+	if(actor == '*'){
+		result = arg1*arg2;
+	}
+	else if(actor == '/'){
+		result = arg1/arg2;
+	}
+	else if(actor == '+'){
+		result = arg1+arg2;
+	}
+	else if(actor == '-'){
+		result = arg1-arg2;
+	}
+	else if(actor == '%'){
+		result = arg1%arg2;
+	}
+	disp.textContent = '';
+	disp.appendChild(document.createTextNode(result));
 }
